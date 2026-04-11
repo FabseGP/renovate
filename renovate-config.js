a9087a49f685f78269d45bb9a9db445239e3ca26
fabseman <personal@fabseman.space>
2026-04-11 23:42:20 +0200

    feat: initial commit

---
+++ renovate-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  branchPrefix: 'renovate/',
+  username: 'renovate-release',
+  gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
+  platform: 'github',
+  repositories: [
+    'FabseGP/aur-packages',
+  ]
+};
