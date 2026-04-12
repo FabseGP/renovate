5fa92ddce6768881550ee83a640da630b0c626ee
fabseman <personal@fabseman.space>
2026-04-12 21:56:35 +0200

    fix: add onboarding

--- renovate-config.js
+++ renovate-config.js
@@ -1,8 +1,10 @@
 module.exports = {
   branchPrefix: 'renovate/',
   gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
-  onboarding: false,
+  onboarding: true,
   platform: 'github',
+  extends: ["config:recommended"],
+  onboardingRebaseCheckbox: true,
   repositories: [
     'FabseGP/aur-packages',
   ]
