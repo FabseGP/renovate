dd933865a5837ba4c028426b168ce7fdf4441fc6
fabseman <personal@fabseman.space>
2026-04-13 11:43:25 +0200

    fix: persist repo data

--- renovate-config.js
+++ renovate-config.js
@@ -2,6 +2,8 @@ module.exports = {
   branchPrefix: 'renovate/',
   gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
   onboarding: true,
+	persistRepoData: true,
+	optimizeForDisabled: true,
   platform: 'github',
   extends: ["config:recommended"],
   onboardingRebaseCheckbox: true,
